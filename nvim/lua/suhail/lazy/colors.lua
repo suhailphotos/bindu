592f1a4949ed61529566bd09dc3ec3258525f86e
Suhail <suhailece@gmail.com>
2025-09-01 16:37:53 -0700

    update to mistbloom

--- nvim/lua/suhail/lazy/colors.lua
+++ nvim/lua/suhail/lazy/colors.lua
@@ -34,8 +34,8 @@ Tip: run :LilacList to see all IDs, e.g. lilac-pearlbloom, lilac-nightbloom,
 ]]
 
 local DEFAULT_PALETTES = {
-  light = "lilac-pearlbloom",
-  dark  = "lilac-nightbloom",
+  light = "lilac-mistbloom",
+  dark  = "lilac-mistbloom",
 }
 
 -- Optional per-session overrides via env (great over SSH)
