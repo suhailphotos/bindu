2bca6246716c9af997cce2d21fab4af2bc963b0f
Suhail Suhail <suhailece@gmail.com>
2025-12-16 22:06:46 -0800

    Update neovim to use 4 tab stops for Rust

--- nvim/lua/suhail/set.lua
+++ nvim/lua/suhail/set.lua
@@ -46,3 +46,16 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = "*",
   command = [[%s/\s\+$//e]],
 })
+
+-- Rust: 4-space indent (matches rustfmt)
+vim.api.nvim_create_autocmd("FileType", {
+  group = SuhailGroup,          -- reuse your existing group
+  pattern = "rust",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.expandtab = true
+  end,
+  desc = "Rust: use 4-space indent",
+})
